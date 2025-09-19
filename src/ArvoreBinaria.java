f7084e8d696d5edd7ecd8540aaa758be7e7aa3b0
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-09-19 09:50:12 -0300

    Algoritmos para percorrer a árvore

--- src/ArvoreBinaria.java
+++ src/ArvoreBinaria.java
@@ -1,3 +1,21 @@
 public class ArvoreBinaria {
     private No raiz;
+
+    public void preordem(No y){
+        System.out.print(this.raiz.getInfo() + " ");
+        preordem(this.raiz.getEsquerda());
+        preordem(this.raiz.getDireita());
+    }
+
+    public void inordem(No y){
+        inordem(this.raiz.getEsquerda());
+        System.out.print(this.raiz.getInfo() + " ");
+        inordem(this.raiz.getDireita());
+    }
+
+    public void posordem(No y){
+        posordem(this.raiz.getEsquerda());
+        posordem(this.raiz.getDireita());
+        System.out.print(this.raiz.getInfo() + " ");
+    }
 }
