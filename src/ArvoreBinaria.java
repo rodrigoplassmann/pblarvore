7ef9eb46c270c57ea0b86ecea7c18fdbd0fe4b07
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-09-19 10:01:08 -0300

    Método para adicionar elementos

--- src/ArvoreBinaria.java
+++ src/ArvoreBinaria.java
@@ -1,6 +1,24 @@
 public class ArvoreBinaria {
     private No raiz;
 
+    public void insere(int info){
+        this.raiz = inserearvore(this.raiz, info);
+    }
+
+    public No inserearvore(No x, int info){
+        if(this.raiz == null){
+            this.raiz = new No(info);
+        }
+        else if(info < this.raiz.getInfo()){
+            this.raiz.setEsquerda(inserearvore(this.raiz.getEsquerda(), info));
+        }
+        else if(info >= this.raiz.getInfo()){
+            this.raiz.setDireita(inserearvore(this.raiz.getDireita(), info));
+        }
+
+        return this.raiz;
+    }
+
     public void preordem(No y){
         System.out.print(this.raiz.getInfo() + " ");
         preordem(this.raiz.getEsquerda());
